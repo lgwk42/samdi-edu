806dac4411d0f850c7f3ba965dda796c4e04e180
lgwk42 <luisgunkim@gmail.com>
2023-07-04 17:05:47 +0900

    게시판 검색 기능, 리스트에서 작성폼 바로 이동하는 기능 추가

--- src/main/java/com/study/board/controller/BoardController.java
+++ src/main/java/com/study/board/controller/BoardController.java
@@ -14,33 +14,42 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
 @Controller
 public class BoardController {
     @Autowired
     private BoardService boardService;
 
     @GetMapping("/board/write") // localhost:8080/board/write
-    public String boardWriteForm(){
+    public String boardWriteForm() {
 
         return "boardWrite";
     }
 
     @PostMapping("/board/writepro")
     public String boardWritePro(Board board, Model model, MultipartFile file) throws Exception {
+
         boardService.write(board, file);
 
-        model.addAttribute("message","글 작성이 완료되었습니다.");
-        model.addAttribute("searchUrl","/board/list");
+        model.addAttribute("message", "글 작성이 완료되었습니다.");
+        model.addAttribute("searchUrl", "/board/list");
 
         return "message";
     }
-    @GetMapping("/board/list")
-    public String boardList(Model model, @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable){
 
-        Page<Board> list = boardService.boardList(pageable);
+    @GetMapping("/board/list")
+    public String boardList(Model model,
+                            @PageableDefault(page = 0, size = 10, sort = "id", direction = Sort.Direction.DESC) Pageable pageable,
+                            String searchKeyword) {
+
+        Page<Board> list = null;
+
+        if (searchKeyword == null) {
+            list = boardService.boardList(pageable);
+        }else {
+            list = boardService.boardSearchList(searchKeyword,pageable);
+        }
+
+
 
         model.addAttribute("list", list);
 
@@ -48,48 +57,52 @@ public class BoardController {
         int startPage = Math.max(nowPage - 4, 1);
         int endPage = Math.min(nowPage + 5, list.getTotalPages());
 
-        model.addAttribute("list",list);
-        model.addAttribute("nowPage",nowPage);
-        model.addAttribute("startPage",startPage);
-        model.addAttribute("endPage",endPage);
+        model.addAttribute("list", list);
+        model.addAttribute("nowPage", nowPage);
+        model.addAttribute("startPage", startPage);
+        model.addAttribute("endPage", endPage);
 
         return "boardList";
     }
-    
+
     @GetMapping("/board/view")
-    public String boardView(Model model, Integer id){
+    public String boardView(Model model, Integer id) {
+
         model.addAttribute("board", boardService.boardView(id));
 
         return "boardView";
     }
 
     @GetMapping("/board/delete")
-    public String boardDelete(Integer id, Model model){
+    public String boardDelete(Integer id, Model model) {
+
         boardService.boardDelete(id);
 
-        model.addAttribute("message","글이 삭제되었습니다.");
-        model.addAttribute("searchUrl","/board/list");
+        model.addAttribute("message", "글이 삭제되었습니다.");
+        model.addAttribute("searchUrl", "/board/list");
 
         return "message";
     }
 
     @GetMapping("/board/modify/{id}")
-    public String boardModify(@PathVariable("id") Integer id, Model model){
-        model.addAttribute("board",boardService.boardView(id));
+    public String boardModify(@PathVariable("id") Integer id, Model model) {
+
+        model.addAttribute("board", boardService.boardView(id));
 
         return "boardModify";
     }
 
     @PostMapping("/board/update/{id}")
     public String boardUpdate(@PathVariable("id") Integer id, Board board, MultipartFile file, Model model) throws Exception {
+
         Board boardTemp = boardService.boardView(id);
         boardTemp.setTitle(board.getTitle());
         boardTemp.setContent(board.getContent());
 
         boardService.write(boardTemp, file);
 
-        model.addAttribute("message","글이 수정되었습니다");
-        model.addAttribute("searchUrl","/board/list");
+        model.addAttribute("message", "글이 수정되었습니다");
+        model.addAttribute("searchUrl", "/board/list");
 
         return "message";
     }
