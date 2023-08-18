714ead141cc856ff81b79c4f1ea294ea760ab4f1
lgwk42 <luisgunkim@gmail.com>
2023-08-18 10:03:59 +0900

    로그인 기능 수정
    로그인 기능 수정

--- src/main/java/com/study/board/controller/loginController.java
+++ src/main/java/com/study/board/controller/loginController.java
@@ -3,7 +3,6 @@ package com.study.board.controller;
 import com.study.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
